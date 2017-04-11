d27722ee98270cbda26cd972f380300ce42dc835
willzh <willzh@wicresoft.com>
2017-04-11 23:23:39 +0800

    add

---
+++ java8test/src/main/java/willzh/java8test/App.java
@@ -0,0 +1,13 @@
+package willzh.java8test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
