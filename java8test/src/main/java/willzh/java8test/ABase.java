d03b14a0dd5ba9601fd2310af823b5141254f106
willzh <willzh@wicresoft.com>
2017-05-11 19:17:48 +0800

    add refactor

--- java8test/src/main/java/willzh/java8test/ABase.java
+++ java8test/src/main/java/willzh/java8test/ABase.java
@@ -1,9 +1,23 @@
 package willzh.java8test;
 
 public class ABase extends Customer {
-	
+
 	private String fir;
+
+	public void foo1(){
+		printA();
+	}
+
+	private void printA() {
+		System.out.println("aaaa");
+	}
+
+	public void foo2(){
+		printA();
+	}
+
 	
 	
+
 }
 
