4c702b0285cd8060d9dcf172b3ec6e68475ed60b
dev_willzh <willzh@wicresoft.com>
2017-06-06 09:59:41 +0800

    willzh

--- java8test/src/main/java/willzh/java8test/BigDecimalTest.java
+++ java8test/src/main/java/willzh/java8test/BigDecimalTest.java
@@ -6,14 +6,21 @@ public class BigDecimalTest {
 	
 	public static void foo(){
 		
-		int intNumber = 0;
+		int intNumber = 11;
 		System.out.println(new BigDecimal(intNumber));
-		double doubleNumber = 1000.00;
+		double doubleNumber = 1001.00;
 		System.out.println(new BigDecimal(doubleNumber));
 		long longNumber = 1000000000;
 		System.out.println(new BigDecimal(longNumber));
 		String stringNumber = "1111";
 		System.out.println(new BigDecimal(stringNumber));
+		
+		BigDecimal a = new BigDecimal(intNumber);
+		BigDecimal b = new BigDecimal(doubleNumber);
+		System.out.println("a+b=" + a.add(b));
+		System.out.println("b-a=" + b.subtract(a));
+		System.out.println("a*b=" + a.multiply(b));
+		System.out.println("b/a=" + b.divide(a));
 		
 	}
 }
