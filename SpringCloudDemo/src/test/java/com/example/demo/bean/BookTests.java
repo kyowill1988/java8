4ae7eb4f783840d7d0b00797ee00168279b6b4eb
dev_willzh <willzh@wicresoft.com>
2017-07-03 16:37:54 +0800

    add spring boot properties test

---
+++ SpringCloudDemo/src/test/java/com/example/demo/bean/BookTests.java
@@ -0,0 +1,25 @@
+package com.example.demo.bean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.demo.DemoApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class BookTests {
+	
+	
+	@Test
+	public void testBookBean(){
+		
+		Book book = new Book();
+		System.out.println(book.getName());
+		
+	}
+	
+	
+	
+}
